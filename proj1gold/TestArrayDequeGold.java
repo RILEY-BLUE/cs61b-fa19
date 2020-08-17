f6dbfec413c9d945caee8aa01b530f1eca3e691e
RILEY_BLUE <yuruid2@uci.edu>
2020-08-17 09:26:04 +0800

    altered failure sequence.

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -3,12 +3,89 @@
  * @Author: Yurui DU
  * @Description: A rudimentary autograder for project 1A
  * @Date: 20200814 09:58
- * @Version: 0.0
+ * @Version: 1.0
  */
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.ArrayList; // A quick touch to make failure sequences.
+import java.util.List;
 
 public class TestArrayDequeGold {
+    @Test
+    public void testArrayDeque() {
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        // addLast
+        for (int i=0; i<10; i++) {
+            int random = StdRandom.uniform(100);
+            ads.addLast(random);
+            sad.addLast(random);
+        }
+        for (int i=0; i<10; i++) {
+            int actual = ads.get(i);
+            int expected = sad.get(i);
+            assertEquals("Oh noooo!\nThis is bad in addLast():\n   Random number " + actual
+                            + " not equal to " + expected + "!",
+                    expected, actual);
+        }
+
+        // addFirst
+        for (int i=0; i<10; i++) {
+            int random = StdRandom.uniform(100);
+            ads.addFirst(random);
+            sad.addFirst(random);
+        }
+        for (int i=0; i<10; i++) {
+            int actual = ads.get(i);
+            int expected = sad.get(i);
+            assertEquals("Oh noooo!\nThis is bad in addFirst():\n   Random number " + actual
+                            + " not equal to " + expected + "!",
+                    expected, actual);
+        }
+
+        // removeFirst
+        List<Integer> actualList = new ArrayList<>();
+        List<Integer> expectedList = new ArrayList<>();
+        for (int i=0; i<10; i++) {
+            actualList.add(ads.removeFirst());
+            expectedList.add(sad.removeFirst());
+        }
+        for (int i=0; i<10; i++) {
+            int actual = ads.get(i);
+            int expected = sad.get(i);
+            assertEquals("Oh noooo!\nThis is bad in removeFirst():\n   Random number " + actual
+                            + " not equal to " + expected + "!",
+                    expected, actual);
+        }
+        for (int i=0; i<10; i++) {
+            int actual = actualList.get(i);
+            int expected = expectedList.get(i);
+            assertEquals("Oh noooo!\nThis is bad in removeFirst():\n   Random number " + actual
+                            + " not equal to " + expected + "!",
+                    expected, actual);
+        }
+
+
+        // removeLast
+        actualList.clear();
+        expectedList.clear();
+        for (int i=0; i<10; i++) {
+            actualList.add(ads.removeLast());
+            expectedList.add(sad.removeLast());
+        }
+        int actual = ads.size();
+        int expected = sad.size();
+        assertEquals("Oh noooo!\nThis is bad in removeLast():\n   actual size " + actual
+                        + " not equal to " + expected + "!",
+                expected, actual);
+        for (int i=0; i<10; i++) {
+            assertEquals("Oh noooo!\nThis is bad in removeLast():\n   Random number " + actualList.get(i)
+                            + " not equal to " +  expectedList.get(i) + "!",
+                    expectedList.get(i), actualList.get(i));
+        }
+
+    }
+
     @Test
     public void testAutoGrader() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
@@ -39,14 +116,12 @@ public class TestArrayDequeGold {
                 + ")");
 
         //removeFirst
-
         expect = sad2.removeFirst();
         actual = sad1.removeFirst();
         assertEquals("removeFirst()\n", expect, actual);
         System.out.println("removeFirst()");
 
         //removeLast
-
         expect = sad2.removeLast();
         actual = sad1.removeLast();
         assertEquals("removeLast()\n", expect, actual);
